1c67b9b128bd7ff951b7c5eaccdbb73826c187ef
ULiiAn <chenshenyuan@gznb.com>
2017-09-08 18:01:30 +0800

    add

--- easyRbAC-UI/src/main.js
+++ easyRbAC-UI/src/main.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
 import App from './App.vue'
+import Router from 'vue-router'
 
 Vue.use(ElementUI)
 
+
 new Vue({
-  el: '#app',
-  render: h => h(App)
+    el: '#app',
+    render: h => h(App)
 })

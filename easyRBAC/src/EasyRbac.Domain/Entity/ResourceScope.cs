b53ed2ad0aad0f8f2207d6fe497369d1e3a85422
ULiiAn <chenshenyuan@gznb.com>
2017-09-22 17:22:43 +0800

    add

--- easyRBAC/src/EasyRbac.Domain/Entity/ResourceScope.cs
+++ easyRBAC/src/EasyRbac.Domain/Entity/ResourceScope.cs
@@ -1,19 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using SQLinq;
 
 namespace EasyRbac.Domain.Entity
 {
-    public class ResourceScope
+    [SQLinqTable("user_manage_resource_scope")]
+    public class UserManageResourceScope
     {
         public long Id { get; set; }
 
+        [SQLinqColumn(Ignore = true)]
         public UserEntity User { get; set; }
+
+        public long AppId { get; set; }
 
         public bool IncludeChildren { get; set; }
 
+        public string ResourceId { get; set; }
+
+        public long UserId { get; set; }
+
+        [SQLinqColumn(Ignore = true)]
         public ApplicationEntity Application { get; set; }
 
+        [SQLinqColumn(Ignore = true)]
         public List<AppResourceEntity> Resources { get; set; }
     }
 }

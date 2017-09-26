2c6f86b2e01d5304bde61c0c71ea254255b9ea17
ULiiAn <chenshenyuan@gznb.com>
2017-09-26 13:40:29 +0800

    添加了神符！！！！！

--- easyRbAC-UI/src/main.js
+++ easyRbAC-UI/src/main.js
@@ -10,7 +10,28 @@ import treeMenu from './component/treeMenu.vue'
 import VueClipboard from 'vue-clipboard2'
 import searchLst from './component/commons/searchLst'
 import appResource from './component/commons/appResource'
-
+//
+//                            _ooOoo_
+//                           o8888888o
+//                           88" . "88
+//                           (| -_- |)
+//                           O\  =  /O
+//                        ____/`---'\____
+//                      .'  \\|     |//  `.
+//                     /  \\|||  :  |||//  \
+//                    /  _||||| -:- |||||-  \
+//                    |   | \\\  -  /// |   |
+//                    | \_|  ''\---/''  |   |
+//                    \  .-\__  `-`  ___/-. /
+//                  ___`. .'  /--.--\  `. . __
+//               ."" '<  `.___\_<|>_/___.'  >'"".
+//              | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+//              \  \ `-.   \_ __\ /__ _/   .-` /  /
+//         ======`-.____`-.___\_____/___.-`____.-'======
+//                            `=---='
+//        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+//                      佛祖保佑       永无BUG
+//                      心外无法       法外无心
 
 Vue.use(ElementUI)
 Vue.use(VueRouter);

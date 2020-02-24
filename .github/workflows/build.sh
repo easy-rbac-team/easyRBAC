47ee729f37ce5a8604ed65d77dbdc8f8c627af3a
uliian <yncxcxz@126.com>
2020-02-24 15:56:50 +0800

    add cache

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -1,5 +1,5 @@
 dotnet build easyRBAC/easyRBAC.sln --configuration Release
-dotnet publish easyRBAC/src/EasyRbac.Web -c Release  --self-contained -r linux-x64
+dotnet publish easyRBAC/src/EasyRbac.Web/EasyRbac.Web.csproj -c Release  --self-contained -r linux-x64
 rm -f ./easyRBAC/src/EasyRbac.Web/bin/Release/netcoreapp2.2/linux-x64/publish/appsettings.*
 rm -f ./easyRBAC/src/EasyRbac.Web/bin/Release/netcoreapp2.2/linux-x64/publish/*.pdb
 tar -czvf easyRBAC.tar.gz easyRBAC/src/EasyRbac.Web/bin/Release/netcoreapp2.2/linux-x64/publish
